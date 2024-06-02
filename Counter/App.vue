a526ee8d2dfae9ed8dca5f4f6979109194ea61f1
Naga Malleswari Chennamsetti <60144643+nagamalli148@users.noreply.github.com>
2024-06-02 17:57:51 +0530

    counter in vue

---
+++ Counter/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {ref} from 'vue'
+
+const count=ref(0);
+
+</script>
+
+<template>
+<main>
+  <div>
+    <h4>
+      The current count is ..
+    </h4>
+  <h1>{{count}} </h1>
+  
+  <button @click="count++"> + </button>
+  <button @click="count--"> - </button>
+
+  </div>
+</main>
+</template>
+
+<style scoped>
+
+main{
+  width: 100vw;
+  height: 100vh;
+  background-color: aliceblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div
+{
+  text-align: center;
+}
+</style>
